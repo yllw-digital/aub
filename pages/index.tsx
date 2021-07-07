304c92f5c329fbd3a167f75cbb8d3982626eff3e
Joe Abdel Sater <joegabdelsater@hotmail.com>
2021-07-07 14:02:13 +0300

    added logos

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,19 @@ import Layout from '../components/Layout';
 
 export default function Home() {
   return (
-    <Layout>
-        <img
+    <div>
+      <Layout>
+        {/* <img
           src="/map.png"
           alt="Picture of the author"
-        />
-    </Layout>
+        /> */}
+        <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1mg2lV3nlrMZrJlNoLZuNeFIjv08&ie=UTF8&hl=en&msa=0&ll=47.672943227146995%2C-122.298856&spn=0.009738%2C0.018303&output=embed&z=16" style={{
+          width: '100%',
+          height: 'calc(100vh - 220px)'
+        }} />
+      </Layout>
+      <img src="/aub-logo.png" className={styles.aubLogo} />
+      <img src="/beirut-logo.png" className={styles.beirutLogo}  />
+    </div>
   )
 }
