408969dbe8494ac98f3547c8fc7e5ea6cc4c0087
Joe Abdel Sater <joegabdelsater@hotmail.com>
2021-09-01 08:46:12 +0300

    rendered all filters

--- components/Filters.js
+++ components/Filters.js
@@ -3,9 +3,11 @@ import * as contactStyles from '../styles/Contact.module.css';
 import * as surveyStyles from '../styles/Survey.module.css';
 import { getFilters } from '../services/statistics/statistics'
 import { useEffect, useState } from 'react';
+import { useForm } from "react-hook-form";
 
 export default function Filters() {
     const [filters, setFilters] = useState([]);
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
     useEffect(() => {
         const fetchFilters = async () => {
@@ -13,31 +15,45 @@ export default function Filters() {
             setFilters(filtersRes?.data);
         }
         fetchFilters()
+
+
     }, [])
 
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
+    const renderFields = () => {
+        const keys = Object.keys(filters);
+        let fields = []
+        keys.map((key) => {
+            fields.push(
+                <div className={surveyStyles.formItem} key={key.toString()}>
+                    <label className={`${contactStyles.label}`}>{filters[key].question}</label>
+
+                    <select className={contactStyles.formInput} {...register(filters[key].question_id.toString())}>
+                        <option value="">Set filter</option>
+                        {filters[key].config.options.map((option, index) => {
+                            return <option value={option} key={index.toString()}>{option}</option>
+                        })}
+                    </select>
+                </div>
+            )
+
+        })
+        return fields;
+    }
 
     return (
         <div className={surveyStyles.filtersOverlay}>
             <div className={surveyStyles.filtersContainer}>
-
-                <form>
-
-                    <div className={surveyStyles.formItem}>
-                        <label className={`${contactStyles.label}`}>Filter name</label>
-
-                        <select className={contactStyles.formInput}>
-                            <option value="">Specify Option</option>
-                        </select>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className={surveyStyles.fieldsContainer}>
+                        {renderFields()}
                     </div>
-
-                    <div className={surveyStyles.formItem}>
-                        <label className={`${contactStyles.label}`}>Filter name</label>
-
-                        <select className={contactStyles.formInput}>
-                            <option value="">Specify Option</option>
-                        </select>
+                    <div className={surveyStyles.thirdGrid} style={{ display: 'block' }}>
+                        <button type="submit" className={contactStyles.submitBtn}>SUBMIT</button>
                     </div>
-
                 </form>
 
             </div>
