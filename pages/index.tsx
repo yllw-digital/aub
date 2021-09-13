16e13dd66ba774879e8982ea87937e5887e7d6ae
Joe Abdel Sater <joegabdelsater@hotmail.com>
2021-09-13 14:50:59 +0300

    replaced with real map

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 
   return (
     <Layout>
-      <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1mg2lV3nlrMZrJlNoLZuNeFIjv08&ie=UTF8&hl=en&msa=0&ll=47.672943227146995%2C-122.298856&spn=0.009738%2C0.018303&output=embed&z=16" className={styles.map} />
+      <iframe src="https://experience.arcgis.com/experience/e25736eb4cf54344abb4a3bbfde809b5/" className={styles.map} />
     </Layout>
   )
 
