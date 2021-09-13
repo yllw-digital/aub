35a6b801041a6e4b88004216bd0a2e47440cc616
Joe Abdel Sater <joegabdelsater@hotmail.com>
2021-09-13 17:05:54 +0300

    changed to tedmob url

--- services/url.js
+++ services/url.js
@@ -1,4 +1,4 @@
 
 // export const URL = "http://localhost:8000" //BASE URL, must NOT end with /
-export const URL = "http://aub.yllw.tech" //BASE URL, must NOT end with /
+export const URL = "http://aub-backend.tedmob.com" //BASE URL, must NOT end with /
 
