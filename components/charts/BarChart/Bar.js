29fc2247605959d9100a783a0d6f19235e7ab58e
Joe Abdel Sater <joegabdelsater@hotmail.com>
2021-09-14 13:25:27 +0300

    finished removing styles

--- components/charts/BarChart/Bar.js
+++ components/charts/BarChart/Bar.js
@@ -1,14 +1,14 @@
 
-import styles from '../../../styles/BarChart.module.css';
+// import styles from '../../../styles/BarChart.module.css';
 
 export default function Bar(props) {
     const { percentage, count, color } = props;
     return (
         <div style={{ textAlign: 'center', height: 150, display: 'flex', flexDirection: 'column', justifyContent:'flex-end' }}>
-            <div className={`${styles.bar}`} style={{ height: `${percentage}%`, backgroundColor: color }}>
-                <p className={styles.count}>{count}</p>
+            <div className='bar' style={{ height: `${percentage}%`, backgroundColor: color }}>
+                <p className='count'>{count}</p>
             </div>
-            <p className={styles.legendBottom}>Size 1</p>
+            <p className='legendBottom'>Size 1</p>
         </div>
     )
 }
