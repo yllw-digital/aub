560caca40339bf9ff051c999abb5ee3d63ba776f
Andy Abi Haidar <andy@yllwdigital.com>
2022-03-06 19:56:42 +0400

    new fixes

--- services/url.js
+++ services/url.js
@@ -1,6 +1,6 @@
 
 // export const URL = "http://localhost:8000" //BASE URL, must NOT end with /
 // export const URL = "http://192.168.1.2:8000"
-// export const URL = "https://aub-backend.tedmob.com" //BASE URL, must NOT ecnd with /
-export const URL = "http://localhost" //BASE URL, must NOT ecnd with /
+export const URL = "https://aub-backend.tedmob.com" //BASE URL, must NOT ecnd with /
+// export const URL = "http://localhost" //BASE URL, must NOT ecnd with /
 
